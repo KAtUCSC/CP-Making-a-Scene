f706ea16602ed06f7ef003bf4bce8322fc7be3f4
Kira W <smway@ucsc.edu>
2024-01-17 15:43:12 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Kira Way
+// Date: 2024-1-17
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [MainMenu, Play]
+}
+
+let game = new Phaser.Game(config)
